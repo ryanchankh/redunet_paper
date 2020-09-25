ff582e5afc8d0b5cc8acf4c73626d3097731ca4b
ryanchankh <ryanchankh@berkeley.edu>
2020-09-25 15:57:11 -0700

    change cmds

--- scripts/run_mnist_rotation.sh
+++ scripts/run_mnist_rotation.sh
@@ -1,1 +1,1 @@
-python3 mnist_rotation.py --samples 10000 --time 200 --kernel 5 --channels 10 --layers 1000 --eps 0.1 --eta 0.5 --lmbda 5000 --relu True
+python3 mnist_rotation.py --samples 2000 --time 200 --kernel 5 --channels 10 --layers 1000 --eps 0.1 --eta 0.5 --lmbda 5000 --relu True

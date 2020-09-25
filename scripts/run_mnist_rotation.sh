217f5601ef6e605144668d400c9723426034f7b2
ryanchankh <ryanchankh@berkeley.edu>
2020-09-25 02:08:00 -0700

    update scripts

--- scripts/run_mnist_rotation.sh
+++ scripts/run_mnist_rotation.sh
@@ -1,2 +1,1 @@
-python3 mnist_rotation.py --samples 10000 --time 50 --kernel 5 --channels 10 --layers 500 --eps 0.1 --eta 0.1 --lmbda 5000 --relu True
-
+python3 mnist_rotation.py --samples 10000 --time 200 --kernel 5 --channels 10 --layers 1000 --eps 0.1 --eta 0.5 --lmbda 5000 --relu True

379d6e654f76014a248010f975043dc2edeefb66
ryanchankh <ryanchankh@berkeley.edu>
2020-09-25 15:56:45 -0700

    rename scripts and files

---
+++ scripts/run_gaussian3d.sh
@@ -0,0 +1,7 @@
+python3 gaussian3d.py --data 1 --noise 0.1 --samples 1000 --layers 500 --eta 0.1 --eps 0.1
+
+# python3 gaussian3d.py --data 2 --noise 0.1 --samples 1000 --layers 500 --eta 0.1 --eps 0.1
+# python3 gaussian3d.py --data 2 --noise 0.2 --samples 1000 --layers 500 --eta 0.1 --eps 0.1
+# python3 gaussian3d.py --data 2 --noise 0.3 --samples 1000 --layers 500 --eta 0.1 --eps 0.1
+# python3 gaussian3d.py --data 2 --noise 0.4 --samples 1000 --layers 500 --eta 0.1 --eps 0.1
+# python3 gaussian3d.py --data 2 --noise 0.5 --samples 1000 --layers 500 --eta 0.1 --eps 0.1

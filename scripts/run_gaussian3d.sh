49e1cb20f93fb872fc48a045a2f908f2d03683b9
ryanchankh <ryanchankh@berkeley.edu>
2020-09-25 18:22:06 -0700

    update scripts

--- scripts/run_gaussian3d.sh
+++ scripts/run_gaussian3d.sh
@@ -1,7 +1,3 @@
-python3 gaussian3d.py --data 1 --noise 0.1 --samples 1000 --layers 500 --eta 0.1 --eps 0.1
+# python3 gaussian3d.py --data 1 --noise 0.2 --samples 500 --layers 750 --eta 0.1 --eps 0.1
 
-# python3 gaussian3d.py --data 2 --noise 0.1 --samples 1000 --layers 500 --eta 0.1 --eps 0.1
-# python3 gaussian3d.py --data 2 --noise 0.2 --samples 1000 --layers 500 --eta 0.1 --eps 0.1
-# python3 gaussian3d.py --data 2 --noise 0.3 --samples 1000 --layers 500 --eta 0.1 --eps 0.1
-# python3 gaussian3d.py --data 2 --noise 0.4 --samples 1000 --layers 500 --eta 0.1 --eps 0.1
-# python3 gaussian3d.py --data 2 --noise 0.5 --samples 1000 --layers 500 --eta 0.1 --eps 0.1
+python3 gaussian3d.py --data 2 --noise 0.2 --samples 500 --layers 500 --eta 0.1 --eps 0.1

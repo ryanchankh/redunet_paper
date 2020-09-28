b6c8ee1c12a5f60c3183d9a502f656d75b65da43
ryanchankh <ryanchankh@berkeley.edu>
2020-09-28 16:55:48 -0700

    remove time

--- scripts/run_mnist_rotation.sh
+++ scripts/run_mnist_rotation.sh
@@ -1,2 +1,2 @@
-python3 mnist_rotation.py --classes 01 --samples 1000 --time 200  --layers 3000 --eps 0.1 --eta 0.5 --lmbda 5000
-python3 mnist_rotation.py --classes 37 --samples 1000 --time 200  --layers 3000 --eps 0.1 --eta 0.5 --lmbda 5000
+python3 mnist_rotation.py --classes 01 --samples 1000 --layers 3000 --eps 0.1 --eta 0.5 --lmbda 5000
+python3 mnist_rotation.py --classes 37 --samples 1000 --layers 3000 --eps 0.1 --eta 0.5 --lmbda 5000

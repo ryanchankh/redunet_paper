669b75b172a068ed33fc3a5534d51a05e4e08460
ryanchankh <ryanchankh@berkeley.edu>
2020-09-28 16:11:30 -0700

    update scripts

--- scripts/run_mnist_rotation.sh
+++ scripts/run_mnist_rotation.sh
@@ -1,1 +1,2 @@
-python3 mnist_rotation.py --samples 2000 --time 200 --kernel 5 --channels 7 --layers 1500 --eps 0.1 --eta 0.5 --lmbda 5000 --relu True
+python3 mnist_rotation.py --classes 01 --samples 1000 --time 200  --layers 3000 --eps 0.1 --eta 0.5 --lmbda 5000
+python3 mnist_rotation.py --classes 37 --samples 1000 --time 200  --layers 3000 --eps 0.1 --eta 0.5 --lmbda 5000
---
+++ scripts/run_sinusoids.sh
@@ -0,0 +1,1 @@
+python3 sinusoid.py --data 7 --samples 200 --time 150 --kernel 3 --channels 7 --layers 2000 --eps 0.1 --eta 0.1

dd7ab8b0b586d65b24b5fbc6f2b8ecaf906fdab5
ryanchankh <ryanchankh@berkeley.edu>
2020-09-29 01:11:41 -0700

    update plots for paper

--- mnist_rotation.py
+++ mnist_rotation.py
@@ -18,7 +18,7 @@ import utils
 # hyperparameters
 parser = argparse.ArgumentParser()
 parser.add_argument('--samples', type=int, help="number of samples for initialization")
-parser.add_argument('--classes', type=str, help="Classes to Keep (Example: '37', '01")
+parser.add_argument('--classes', nargs="+", type=int, help="Classes to Keep (Example: 0 1)")
 parser.add_argument('--layers', type=int, help="number of layers")
 parser.add_argument('--eta', type=float, help='learning rate')
 parser.add_argument('--eps', type=float, help='eps squared')
@@ -29,7 +29,7 @@ parser.add_argument('--save_dir', type=str, default='./saved_models/',
                     help='base directory for saving PyTorch model. (default: ./saved_models/)')
 args = parser.parse_args()
 
-model_dir = os.path.join(args.save_dir, f"mnist_rotation-classes{args.classes}",
+model_dir = os.path.join(args.save_dir, f"mnist_rotation-classes{''.join(map(str, args.classes))}",
                          "samples{}_layers{}_eps{}_eta{}{}"
                          "".format(args.samples, args.layers, args.eps, args.eta, args.tail))
 os.makedirs(model_dir, exist_ok=True)
@@ -42,18 +42,15 @@ X_train = np.load('./data/mnist_rotation/mnist_X_train_C5_T200.npy')
 y_train = np.load('./data/mnist_rotation/mnist_y_train_C5_T200.npy')
 X_test = np.load('./data/mnist_rotation/mnist_X_test_C5_T200.npy')
 y_test = np.load('./data/mnist_rotation/mnist_y_test_C5_T200.npy')
-classes = np.array(list(args.classes)).astype(np.int32)
-num_classes = classes.size
-X_train, y_train = tf.filter_class(X_train, y_train, classes, args.samples)
+X_train, y_train = tf.filter_class(X_train, y_train, args.classes, args.samples)
 X_train, y_train = tf.shuffle(X_train, y_train)
-X_test, y_test = tf.filter_class(X_test, y_test, classes, args.samples)
+X_test, y_test = tf.filter_class(X_test, y_test, args.classes, args.samples)
 X_each, y_each = tf.get_n_each(X_test, y_test, 5)
 X_translate, y_translate = tf.translate_all(X_each, y_each)
-print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
 # setup architecture
 layers = [Fourier1D(args.layers, eta=args.eta, eps=args.eps)]
-model = Architecture(layers, model_dir, num_classes)
+model = Architecture(layers, model_dir, len(args.classes))
 
 # train/test pass
 print("Forward pass - train features")
@@ -65,17 +62,6 @@ utils.save_loss(model.loss_dict, model_dir, "test")
 print("Forward pass - translated features")
 Z_translate = model(X_translate).real
 utils.save_loss(model.loss_dict, model_dir, "translate")
-
-# load features
-# X_train = np.load(os.path.join(model_dir, "features", "X_train_features.npy"))
-# X_test = np.load(os.path.join(model_dir, "features", "X_test_features.npy"))
-# X_translate = np.load(os.path.join(model_dir, "features", "X_translate_features.npy"))
-# Z_train = np.load(os.path.join(model_dir, "features", "Z_train_features.npy"))
-# Z_test = np.load(os.path.join(model_dir, "features", "Z_test_features.npy"))
-# Z_translate = np.load(os.path.join(model_dir, "features", "Z_translate_features.npy"))
-# y_train = np.load(os.path.join(model_dir, "features", "Z_train_labels.npy"))
-# y_test = np.load(os.path.join(model_dir, "features", "Z_test_labels.npy"))
-# y_translate = np.load(os.path.join(model_dir, "features", "Z_translate_labels.npy"))
 
 # save features
 utils.save_features(model_dir, "X_train", X_train, y_train)
@@ -95,23 +81,23 @@ Z_translate = tf.normalize(Z_translate.reshape(Z_translate.shape[0], -1))
 # evaluation test
 _, acc_svm = evaluate.svm(Z_train, y_train, Z_test, y_test)
 acc_knn = evaluate.knn(Z_train, y_train, Z_test, y_test, k=5)
-acc_svd = evaluate.nearsub(Z_train, y_train, Z_test, y_test, classes, n_comp=50)
+acc_svd = evaluate.nearsub(Z_train, y_train, Z_test, y_test, len(args.classes), n_comp=100)
 acc = {"svm": acc_svm, "knn": acc_knn, "nearsub-svd": acc_svd} 
 utils.save_params(model_dir, acc, name="acc_test.json")
 
 # evaluation translate
 _, acc_svm = evaluate.svm(Z_train, y_train, Z_translate, y_translate)
 acc_knn = evaluate.knn(Z_train, y_train, Z_translate, y_translate, k=5)
-acc_svd = evaluate.nearsub(Z_train, y_train, Z_translate, y_translate, classes, n_comp=50)
+acc_svd = evaluate.nearsub(Z_train, y_train, Z_translate, y_translate, len(args.classes), n_comp=100)
 acc = {"svm": acc_svm, "knn": acc_knn, "nearsub-svd": acc_svd} 
 utils.save_params(model_dir, acc, name="acc_translate.json")
 
 # plot
 plot.plot_combined_loss(model_dir)
-plot.plot_heatmap(X_train, y_train, "X_train", classes, model_dir)
-plot.plot_heatmap(X_test, y_test, "X_test", classes, model_dir)
-plot.plot_heatmap(X_translate, y_translate, "X_translate", classes, model_dir)
-plot.plot_heatmap(Z_train, y_train, "Z_train", classes, model_dir)
-plot.plot_heatmap(Z_test, y_test, "Z_test", classes, model_dir)
-plot.plot_heatmap(Z_translate, y_translate, "Z_translate", classes, model_dir)
-plot.plot_nearsub_angle(X_train, y_train, Z_train, X_translate, y_translate, Z_translate, 50, model_dir)
+plot.plot_heatmap(X_train, y_train, "X_train", len(args.classes), model_dir)
+plot.plot_heatmap(X_test, y_test, "X_test", len(args.classes), model_dir)
+plot.plot_heatmap(X_translate, y_translate, "X_translate", len(args.classes), model_dir)
+plot.plot_heatmap(Z_train, y_train, "Z_train", len(args.classes), model_dir)
+plot.plot_heatmap(Z_test, y_test, "Z_test", len(args.classes), model_dir)
+plot.plot_heatmap(Z_translate, y_translate, "Z_translate", len(args.classes), model_dir)
+plot.plot_nearsub_angle(X_train, y_train, Z_train, X_translate, y_translate, Z_translate, 100, model_dir)
--- plot.py
+++ plot.py
@@ -5,7 +5,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from torch.nn.functional import normalize
 from mpl_toolkits.mplot3d import Axes3D
-from MulticoreTSNE import MulticoreTSNE as TSNE
 from sklearn.decomposition import TruncatedSVD, PCA
 import train_func as tf
 import utils
@@ -14,25 +13,27 @@ import utils
 def plot_heatmap(features, labels, title, classes, model_dir):
     """Plot heatmap of cosine simliarity for all features. """
     num_samples = features.shape[0]
-    if type(classes) == int:
-        classes = np.arange(classes)
+    classes = np.arange(np.unique(labels).size)
     features_sort_, _ = utils.sort_dataset(features, labels, 
                             classes=classes, stack=True)
     sim_mat = np.abs(features_sort_ @ features_sort_.T)
     print(sim_mat.min(), sim_mat.max())
 
+    plt.rc('text', usetex=True)
+    plt.rcParams['font.family'] = 'serif'
+    plt.rcParams['font.serif'] = ['Times New Roman']
     fig, ax = plt.subplots(figsize=(7, 5), sharey=True, sharex=True, dpi=400)
     im = ax.imshow(sim_mat, cmap='Blues')
     fig.colorbar(im, pad=0.02, drawedges=0, ticks=[0, 0.5, 1])
-    ax.set_xticks(np.linspace(0, num_samples, len(classes)))
-    ax.set_yticks(np.linspace(0, num_samples, len(classes)))
-    [tick.label.set_fontsize(12) for tick in ax.xaxis.get_major_ticks()] 
-    [tick.label.set_fontsize(12) for tick in ax.yaxis.get_major_ticks()]
+    ax.set_xticks(np.linspace(0, num_samples, len(classes)+1))
+    ax.set_yticks(np.linspace(0, num_samples, len(classes)+1))
+    [tick.label.set_fontsize(14) for tick in ax.xaxis.get_major_ticks()] 
+    [tick.label.set_fontsize(14) for tick in ax.yaxis.get_major_ticks()]
     fig.tight_layout()
     
     save_dir = os.path.join(model_dir, "figures", "heatmaps")
     os.makedirs(save_dir, exist_ok=True)
-    plt.savefig(os.path.join(save_dir, f"heatmap-{title}.png"), dpi=200)
+    plt.savefig(os.path.join(save_dir, f"heatmap-{title}.pdf"), dpi=200)
     plt.close()
 
 def plot_combined_loss(model_dir):
@@ -63,7 +64,7 @@ def plot_combined_loss(model_dir):
     
     save_dir = os.path.join(model_dir, 'figures', 'loss')
     os.makedirs(save_dir, exist_ok=True)
-    file_name = os.path.join(save_dir, f'loss-traintest.png')
+    file_name = os.path.join(save_dir, f'loss-traintest.pdf')
     plt.savefig(file_name, dpi=200)
     plt.close()
 
@@ -71,24 +72,47 @@ def plot_2d(Z, y, name, model_dir):
     plot_dir = os.path.join(model_dir, "figures", "2dscatter")
     colors = np.array(['green', 'red', 'blue'])
     os.makedirs(plot_dir, exist_ok=True)
-    plt.scatter(Z[:, 0], Z[:, 1], c=colors[y], alpha=0.8)
-    # plt.title(name)
-    plt.ylim(-1.2, 1.2)
-    plt.xlim(-1.2, 1.2)
-    plt.xticks([-1.0, -0.5, 0.0, 0.5, 1.0])
-    plt.yticks([-1.0, -0.5, 0.0, 0.5, 1.0])
-    plt.savefig(os.path.join(plot_dir, "scatter2d-"+name+".png"), dpi=200)
+    plt.rcParams['font.family'] = 'serif'
+    plt.rcParams['font.serif'] = ['Times New Roman']
+    colors = np.array(['royalblue', 'forestgreen', 'blue'])
+    fig, ax = plt.subplots(figsize=(6, 5), dpi=200)
+    ax.scatter(Z[:, 0], Z[:, 1], c=colors[y], alpha=0.5)
+    ax.scatter(0.0, 0.0, c='black', alpha=0.8, marker='s')
+    # ax.arrow(0.0, 0.0, Z[:, 0], Z[:, 1])
+    ax.set_ylim(-1.2, 1.2)
+    ax.set_xlim(-1.2, 1.2)
+    ax.set_xticks([-1.0, -0.5, 0.0, 0.5, 1.0])
+    ax.set_yticks([-1.0, -0.5, 0.0, 0.5, 1.0])
+    ax.grid(linestyle=':')
+    Z, _ = tf.get_n_each(Z, y, 1)
+    ax.arrow(0, 0, Z[0, 0], Z[0, 1], head_width=0.03, head_length=0.05, fc='k', ec='k', length_includes_head=True)
+    ax.arrow(0, 0, Z[-1, 0], Z[-1, 1], head_width=0.03, head_length=0.05, fc='k', ec='k', length_includes_head=True)
+    [tick.label.set_fontsize(14) for tick in ax.xaxis.get_major_ticks()] 
+    [tick.label.set_fontsize(14) for tick in ax.yaxis.get_major_ticks()]
+    plt.savefig(os.path.join(plot_dir, "scatter2d-"+name+".pdf"), dpi=200)
     plt.close()
 
-def plot_3d(X, y, name, model_dir):
+def plot_3d(Z, y, name, model_dir):
     colors = np.array(['green', 'blue', 'red'])
     savedir = os.path.join(model_dir, 'figures', '3d')
     os.makedirs(savedir, exist_ok=True)
+    plt.rc('text', usetex=True)
+    plt.rcParams['font.family'] = 'serif'
+    plt.rcParams['font.serif'] = ['Times New Roman']
+    colors = np.array(['forestgreen', 'royalblue', 'brown'])
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=colors[y], cmap=plt.cm.Spectral)
+    ax.scatter(Z[:, 0], Z[:, 1], Z[:, 2], c=colors[y], cmap=plt.cm.Spectral, s=200.0)
+    Z, _ = tf.get_n_each(Z, y, 1)
+    ax.scatter(0.0, 0.0, 0.0, c='black', cmap=plt.cm.Spectral, marker='s', s=50.0)
+    ax.quiver(0.0, 0.0, 0.0, Z[0, 0], Z[0, 1], Z[0, 2], length=1.0, normalize=True, arrow_length_ratio=0.05, color='black')
+    ax.quiver(0.0, 0.0, 0.0, Z[1, 0], Z[1, 1], Z[1, 2], length=1.0, normalize=True, arrow_length_ratio=0.05, color='black')
+    ax.quiver(0.0, 0.0, 0.0, Z[2, 0], Z[2, 1], Z[2, 2], length=1.0, normalize=True, arrow_length_ratio=0.05, color='black')
+    [tick.label.set_fontsize(14) for tick in ax.xaxis.get_major_ticks()] 
+    [tick.label.set_fontsize(14) for tick in ax.yaxis.get_major_ticks()]
+    [tick.label.set_fontsize(14) for tick in ax.zaxis.get_major_ticks()]
     plt.tight_layout()
-    fig.savefig(os.path.join(savedir, f"scatter3d-{name}.png"), dpi=200)
+    fig.savefig(os.path.join(savedir, f"scatter3d-{name}.pdf"), dpi=200)
     plt.close()
 
 def plot_nearsub_angle(X_train, y_train, Z_train, X_test, y_test, Z_test, n_comp, model_dir):
@@ -124,7 +148,7 @@ def plot_nearsub_angle(X_train, y_train, Z_train, X_test, y_test, Z_test, n_comp
         ax.set_ylabel('count')
         ax.set_xlim([0, 1])
         fig.tight_layout()
-        fig.savefig(os.path.join(save_dir, f'subspace_angle-dim{n_comp}-X-subspace{subspace_class}'))
+        fig.savefig(os.path.join(save_dir, f'subspace_angle-dim{n_comp}-X-subspace{subspace_class}.pdf'))
         plt.close()
 
     # with Z
@@ -151,7 +175,7 @@ def plot_nearsub_angle(X_train, y_train, Z_train, X_test, y_test, Z_test, n_comp
         ax.set_ylabel('count')
         ax.set_xlim([0, 1])
         fig.tight_layout()
-        fig.savefig(os.path.join(save_dir, f'subspace_angle-dim{n_comp}-Z-subspace{subspace_class}'))
+        fig.savefig(os.path.join(save_dir, f'subspace_angle-dim{n_comp}-Z-subspace{subspace_class}.pdf'))
         plt.close()
 
 def plot_pca(features, labels, n_comp, title, classes, model_dir):
@@ -189,7 +213,7 @@ def plot_pca(features, labels, n_comp, title, classes, model_dir):
     [tick.label.set_fontsize(12) for tick in ax.yaxis.get_major_ticks()]
     fig.tight_layout()
     
-    file_name = os.path.join(pca_dir, f"pca_{title}")
+    file_name = os.path.join(pca_dir, f"pca_{title}.pdf")
     fig.savefig(file_name)
     plt.close()
 
@@ -202,12 +226,13 @@ if __name__ == "__main__":
     parser.add_argument('--n_comp', type=int, default=50, help='number of components')
 
     parser.add_argument('--loss', action='store_true', help='plot loss from training and testing')
+    parser.add_argument('--scatter', action='store_true', help='plot scatter')
     parser.add_argument('--heatmap', action='store_true', help='plot heatmaps')
     parser.add_argument('--angle', action='store_true', help='plot angle between subspace and samples')
     parser.add_argument('--pca', action='store_true', help='plot pca')
     args = parser.parse_args()
 
-    # load features
+    params = utils.load_params(args.model_dir)
     X_train = np.load(os.path.join(args.model_dir, "features", "X_train_features.npy"))
     y_train = np.load(os.path.join(args.model_dir, "features", "Z_train_labels.npy"))
     Z_train = np.load(os.path.join(args.model_dir, "features", "Z_train_features.npy"))
@@ -216,7 +241,7 @@ if __name__ == "__main__":
     y_test = np.load(os.path.join(args.model_dir, "features", "Z_test_labels.npy"))
     Z_test = np.load(os.path.join(args.model_dir, "features", "Z_test_features.npy"))
 
-    classes = np.array(list(args.classes)).astype(np.int32)
+    classes = np.unique(y_train)
     multichannel = len(X_train.shape) > 2
     X_train = tf.normalize(X_train.reshape(X_train.shape[0], -1))
     X_test = tf.normalize(X_test.reshape(X_test.shape[0], -1))
@@ -234,6 +259,17 @@ if __name__ == "__main__":
     if args.pca:
         plot_pca(X_train, y_train, args.n_comp, "X", classes, args.model_dir)
         plot_pca(Z_train, y_train, args.n_comp, "Z", classes, args.model_dir)
+    if args.scatter:
+        if X_train.shape[1] == 2:
+            plot_2d(X_train, y_train, "X_train", args.model_dir)
+            plot_2d(X_test, y_test, "X_test", args.model_dir)
+            plot_2d(Z_train, y_train, "Z_train", args.model_dir)
+            plot_2d(Z_test, y_test, "Z_test", args.model_dir)
+        elif X_train.shape[1] == 3:
+            plot_3d(X_train, y_train, "X_train", args.model_dir)
+            plot_3d(X_test, y_test, "X_test", args.model_dir)
+            plot_3d(Z_train, y_train, "Z_train", args.model_dir)
+            plot_3d(Z_test, y_test, "Z_test", args.model_dir)
 
     if multichannel: # multichannel data
         X_translate = np.load(os.path.join(args.model_dir, "features", "X_translate_features.npy"))
